8392e68c7e98aeac766a70941cc33263d73aee89
Tigas <tiagomonteirogoncalves@gmail.com>
2018-03-15 21:22:38 +0000

    implemented start to ServerLauncher with portNumber

--- src/org/academiadecodigo/haltistas/Server/Server.java
+++ src/org/academiadecodigo/haltistas/Server/Server.java
@@ -1,4 +1,45 @@
 package org.academiadecodigo.haltistas.Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+    private Set<verySpecialHandler> clientList;
+
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        this.clientList = new HashSet<>();
+    }
+
+    public void start() {
+
+    }
+
+    private void broadcast() {
+        for (verySpecialHandler c : clientList) {
+
+        }
+    }
+
+
+    private class verySpecialHandler implements Runnable {
+        private Socket connection;
+        private PrintWriter toClients;
+        private BufferedReader fromClients;
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
+
+
--- src/org/academiadecodigo/haltistas/ServerLauncher.java
+++ src/org/academiadecodigo/haltistas/ServerLauncher.java
@@ -1,4 +1,27 @@
 package org.academiadecodigo.haltistas;
 
+import org.academiadecodigo.haltistas.Server.Server;
+
+import java.io.IOException;
+
 public class ServerLauncher {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar ServerLauncher <PortNumber>");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        try {
+            Server server = new Server(port);
+            server.start();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
