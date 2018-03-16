9cd3ed67c6a7be51a0a31ba234cdc32667d0fa56
Márcio <marciodanielpinto@gmail.com>
2018-03-16 20:40:36 +0000

    added initial properties, constructor method and addPlayer mathod

--- src/org/academiadecodigo/haltistas/game/PicturusGame.java
+++ src/org/academiadecodigo/haltistas/game/PicturusGame.java
@@ -1,5 +1,25 @@
 package org.academiadecodigo.haltistas.game;
 
+
+import org.academiadecodigo.haltistas.Server.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
 public class PicturusGame {
-    
+
+    private Server server;
+    private List<String> playerList;
+
+    public PicturusGame(Server server) {
+
+        this.server = server;
+        this.playerList = new ArrayList<>();
+    }
+
+    public void addPlayer(String playerName) {
+        playerList.add(playerName);
+    }
+       
 }
