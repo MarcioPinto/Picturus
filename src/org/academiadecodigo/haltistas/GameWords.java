324769ffd302051868c98c7782d94c26d8161be4
Bruno Bento <bruno56389@gmail.com>
2018-03-16 21:52:12 +0000

    add game words

---
+++ src/org/academiadecodigo/haltistas/GameWords.java
@@ -0,0 +1,26 @@
+package org.academiadecodigo.haltistas;
+
+import java.util.*;
+
+public class GameWords {
+
+    private List<String> ListWords = new ArrayList<>();
+
+    public String getWord() {
+
+        String[] words = {"bat", "cat", "ghost", "bug", "snake", "tree"
+                , "knife", "mouse", "sun", "banana", "potato", "duck", "dog"};
+
+        for (int i = 0; i < words.length; i++) {
+
+            ListWords.add(words[i]);
+        }
+        Collections.shuffle(ListWords);
+        System.out.println("shuffle " + ListWords);
+
+        String finalWorld = ListWords.get(0);
+
+        System.out.println(finalWorld);
+        return finalWorld;
+    }
+}
