b3c8694e1c42cad2dc8cb6c14becc3b2497da1d4
Márcio <marciodanielpinto@gmail.com>
2018-03-17 15:55:12 +0000

    moved Gamewords to game package

---
+++ src/org/academiadecodigo/haltistas/game/Main.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.haltistas.game;
+
+import org.academiadecodigo.haltistas.game.GameWords;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    GameWords gameWords = new GameWords();
+    gameWords.getWord();
+    }
+
+}
